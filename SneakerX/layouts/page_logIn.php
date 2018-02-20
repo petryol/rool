7c577c2873bbe278e56e9e2f393d2c0aac912583
ChroustCZ-PC\ChroustCZ <robert.smid02@seznam.cz>
2018-02-20 19:25:50 +0100

    logIn´s page css

--- SneakerX/layouts/page_logIn.php
+++ SneakerX/layouts/page_logIn.php
@@ -11,14 +11,29 @@
 		<div class="darkBG">
 			<div id="logInBox">
 				<div id="logInBoxInside">
-					<span class="logHeader">REGISTRACE</span>
-					<span class="logHeader">PŘIHLÁSIT</span>
+					<div id="logHead">
+						<div class="logHead">
+							<span class="logHeader">REGISTRACE</span>
+						</div>	
+						<div class="logHead">
+							<span class="logHeader">PŘIHLÁSIT</span>
+						</div>	
+					</div>	
 					<div class="logBox"></div>
 					<div class="logBox">
 						<form method="post">
-							<input name="id" type="text">
-							<input name="password" type="password">
-							<input value="Přihlásit" type="submit">
+							<input name="id" type="text" placeholder="USERNAME">
+							<input name="email" type="email" placeholder="E-MAIL">
+							<input name="forname" type="text" placeholder="FORENAME">
+							<input name="surname" type="text" placeholder="SURNAME">
+							<input name="password" type="password" placeholder="PASSWORD">
+							<input name="passwordTry" type="password" placeholder="PASSWORD AGAIN">
+							<input name="country" list="countries" placeholder="COUNTRY">
+							<datalist id="countries">
+								<option value="czech">
+							</datalist>
+							<input type="checkbox" name="acceptTerms"> <span>Souhlasím s pravidly této stránky. <a href="#">Přečíst...</a></span>
+							<input value="Zaregistrovat se" type="submit">
 						</form>
 					</div>
 				</div>
